995b26cee0b28cf011e850000a2a3eab5e427f93
Sriram <trnsriram@gmail.com>
2021-09-11 11:41:06 +0530

    Reorder deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,15 +15,16 @@ module.exports = {
     'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
     'no-use-before-define': ['error', { variables: false }],
     'no-multi-str': 0,
-    'unicorn/prefer-module': 0,
-    'unicorn/no-null': 0,
     // ignore this since eslint is not able to find ts modules that use ESM
     'import/no-unresolved': 0,
     // eslint is unable to find the right extension in an ESM module
     'import/extensions': 0,
+    'import/prefer-default-export': 0,
+    'unicorn/prefer-module': 0,
+    // null is a valid value for initialization
+    'unicorn/no-null': 0,
     'unicorn/prevent-abbreviations': 0,
     'unicorn/prefer-node-protocol': 0,
-    'import/prefer-default-export': 0,
   },
   env: {
     node: true,
