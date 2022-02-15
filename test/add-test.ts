01dae25c1db1a43f36369183689b4795e64fbbc5
Sriram <trnsriram@gmail.com>
2022-02-15 17:25:20 +0530

    Updated deps

--- test/add-test.ts
+++ test/add-test.ts
@@ -2,5 +2,5 @@ import { expect } from 'chai';
 import { add } from '../src/domain/add';
 
 it('should add to numbers from an es module', () => {
-  expect(add(3, 4)).to.equal(9);
+  expect(add(3, 4)).to.equal(7);
 });
